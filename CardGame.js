9db01b60396158a73d7d870264f3d93cd558fc9d
Jay Hong <aquahjh1@gmail.com>
2020-05-28 20:36:40 -0400

    Poker

--- CardGame.js
+++ CardGame.js
@@ -1,47 +1,81 @@
-const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
-const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(",");
+// const suits =            
+// const values = 
+// const rank = 
 
 class Poker {
 
-    constructor(suits, values){
-        this.suits = suits;
-        this.values = values;
+    constructor(suits, values, rank){
+        this.suits = ['hearts', 'diamonds', 'spades', 'clubs']; 
+        this.values = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(",");
+        this.rank = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
         this.deck = [];
         this.hands = {};
+        this.holeCard = [];
+        this.communityCard = [];
     }
-    
-    createDeck = () => {
-        for(let suit of this.suits){
-            for(let value of this.values){
-                this.deck.push({suit: suit, value: value})
+
+    createDeck() {
+        const {suits, values, deck} = this;
+        for(let suit of suits){
+            for(let value of values){
+                deck.push({suit: suit, value: value})
             }
         }
     }
 
     // Fisher-Yactes shuffle
-    shuffle = () => {
-        for(let deckIdx = this.deck.length - 1; deckIdx > 0; deckIdx--){
-            let randNum = Math.floor(Math.random() * this.deck.length);
-            [this.deck[deckIdx], this.deck[randNum]] = [this.deck[randNum], this.deck[deckIdx]];
-
+    shuffle(){
+        const {deck} = this;
+        for(let deckIdx = deck.length - 1; deckIdx > 0; deckIdx--){
+            let randNum = Math.floor(Math.random() * deck.length);
+            [deck[deckIdx], deck[randNum]] = [deck[randNum], deck[deckIdx]];
         }
     }
 
-    drawCard = (numOfCards) => {
-        
+    drawHoleCard(numOfCards = 2){
+        const {deck, holeCard} = this; 
         for(let i = 0; i < numOfCards; i++){
-
-            let card = Math.floor(Math.random() * this.deck.length);
-
-        }
+            let card = deck.pop();
+            holeCard.push(card);
+        }  
     }
 
+    drawCommunityCard() {
+        const {deck, communityCard} = this;
+        for(let i = 0; i < 5; i++){
+            let card = deck.pop();
+            communityCard.push(card);
+        }  
+    }
+
+    clearHand(){
+        const {holeCard} = this;
+        holeCard.splice(0, holeCard.length);
+    }
+
+    checkHand(){
+
+    }
+    // checkHand = (holeCard, communityCard) => {
+
+        
+        // const unique = (value, index, self) => {
+        //     return self.indexOf(value) === index
+        // }
+
+        // const combination = [...holeCard, ...communityCard];            
+
+        // if((combination.filter(unique).length === 1) & (Object.values(combination).filter(n => n === "10" || n ==="J" || n === "Q" || n === "K" || n === "A").length === 5)){
+        //     return true;
+        // }
+    // }
+
 }
 
-class Player extends Poker{
-
-}
-
-class Dealer extends Poker{
-
-}
+const dealer = new Poker()
+dealer.createDeck()
+dealer.shuffle()
+dealer.drawHoleCard()
+dealer.drawCommunityCard()
+// dealer.checkHand()
+dealer.clearHand()
